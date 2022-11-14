69b030c48d2ddd478740722d30ae3b15674647b3
Egor51 <kubrakov_egor87@mail.ru>
2022-11-14 12:17:36 +0300

    initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+
+    private final ConcurrentHashMap<Long, Post> data = new ConcurrentHashMap<>();
+    private final AtomicLong COUNT = new AtomicLong(0);
+
+    public List<Post> all() {
+        //TODO
+        return new ArrayList<>(data.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        //TODO
+        return Optional.ofNullable(data.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(COUNT.incrementAndGet());
+            data.put(post.getId(), post);
+        } else {
+            if (data.containsKey(post.getId()))
+                data.replace(post.getId(), post);
+            else
+                throw new NotFoundException("Failed to update post");
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        data.remove(id);
+    }
+}
---
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -0,0 +1,67 @@
+package ru.netology.servlet;
+
+import ru.netology.controller.PostController;
+import ru.netology.exception.NotFoundException;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    private PostController controller;
+    private static final String GET = "GET";
+    private static final String POST = "POST";
+    private static final String DELETE = "DELETE";
+    private static final String PATH = "/api/posts";
+
+    private static long id = 0;
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+
+            if (path.matches("/api/posts/\\d+")) {
+                id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+            }
+            if (method.equals(GET) && path.equals(PATH)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals(GET) && path.matches("/api/posts/\\d+")) {
+                controller.getById(id, resp);
+                return;
+            }
+            if (method.equals(POST) && path.equals(PATH)) {
+                controller.save(req.getReader(), resp);
+                return;
+            }
+            if (method.equals(DELETE) && path.matches("/api/posts/\\d+")) {
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_CONFLICT);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
